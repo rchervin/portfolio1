78fe638ac0d41e5dfe548850a1764fcab6a72f96
Rachel Lanagan <lanaganinc@gmail.com>
2015-03-26 07:27:58 -0700

    integrated bootstrap and added about page

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,19 +1,37 @@
-$(document).ready(function () {
+// $(document).ready(function () {
 
-$(function() {
-    $('.banner').unslider({
-    speed: 500,               //  The speed to animate each slide (in milliseconds)
-	delay: 2000,              //  The delay between slide animations (in milliseconds)
-	complete: function() {},  //  A function that gets called after every slide animation
-	keys: true,               //  Enable keyboard (left, right) arrow shortcuts
-	dots: true,               //  Display dot navigation
-	fluid: false              //  Support responsive design. May break non-responsive designs
-    });
-});
+// $(function() {
+//     $('.banner').unslider({
+//     speed: 500,               //  The speed to animate each slide (in milliseconds)
+// 	delay: 2000,              //  The delay between slide animations (in milliseconds)
+// 	complete: function() {},  //  A function that gets called after every slide animation
+// 	keys: true,               //  Enable keyboard (left, right) arrow shortcuts
+// 	dots: true,               //  Display dot navigation
+// 	fluid: false              //  Support responsive design. May break non-responsive designs
+//     });
+// });
 
-$('.banner').on("load", function() {
+// $('.banner').on("load", function() {
   	
-});
+// });
 
-});
+// });
 
+// $(document).ready(function(){
+//      $("#carousel").owlCarousel({
+//      	autoPlay: 3000,
+//      });
+// });
+
+// $(document).ready(function() {
+//   $("#owl-example").owlCarousel();
+// });
+
+
+$(document).ready(function(){
+     $("#carousel").carousel({
+     	interval: 2000,
+     	pause: false,
+     	wrap: true,
+     });
+});
